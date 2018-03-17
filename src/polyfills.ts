818e74a99b19a14d1a6c73c4914e71e80b8b8072
Natan Zasępa <zasepanatan@gmail.com>
2018-03-17 17:55:11 +0100

    IE9 polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -77,3 +77,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+import 'ie9-oninput-polyfill';
+import 'console-polyfill';
